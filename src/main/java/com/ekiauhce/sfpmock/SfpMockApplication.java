5c63dbf6e2c34ec151f4aae4f0aa911aac373bbc
ekiauhce <ekiauhce@yandex.ru>
2021-04-15 21:12:32 +0300

    Initial commit

---
+++ src/main/java/com/ekiauhce/sfpmock/SfpMockApplication.java
@@ -0,0 +1,13 @@
+package com.ekiauhce.sfpmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfpMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfpMockApplication.class, args);
+    }
+
+}
