0f50b86f0ffd91b19e06758f81c527315def2d7c
ekiauhce <ekiauhce@yandex.ru>
2021-05-03 16:03:54 +0300

    Add report related objects

---
+++ src/main/java/com/ekiauhce/sfpmock/api/objects/ControlPoint.java
@@ -0,0 +1,40 @@
+
+package com.ekiauhce.sfpmock.api.objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Setter
+@JsonPropertyOrder({
+    "_t",
+    "ControlPointNumber",
+    "Name",
+    "Directions"
+})
+public class ControlPoint {
+
+    @JsonProperty("_t")
+    public String t;
+
+    @JsonProperty("ControlPointNumber")
+    public Integer controlPointNumber;
+
+    @JsonProperty("Name")
+    public String name;
+
+    @JsonProperty("Directions")
+    public List<String> directions;
+
+    public static ControlPoint mock() {
+        ControlPoint controlPoint = new ControlPoint();
+        controlPoint.setT("ControlPoint");
+        controlPoint.setControlPointNumber(2);
+        controlPoint.setName("Еманжелинск");
+        controlPoint.setDirections(Arrays.asList("18_КМ", "ЧЕЛ-ЮЖНЫЙ", "СИНЕГЛАЗОВО"));
+        return controlPoint;
+    }
+}
---
+++ src/main/java/com/ekiauhce/sfpmock/api/objects/TechnovisorReportId.java
@@ -0,0 +1,14 @@
+
+package com.ekiauhce.sfpmock.api.objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({
+    "$numberLong"
+})
+public class TechnovisorReportId {
+
+    @JsonProperty("$numberLong")
+    public String numberLong;
+}
