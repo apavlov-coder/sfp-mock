87b351a23b6930b1ed076f3701f14f8c84f559ca
ekiauhce <ekiauhce@yandex.ru>
2021-05-03 16:08:23 +0300

    Add sendDoc method. Id of Doc and Report will be the same.

--- src/main/java/com/ekiauhce/sfpmock/client/SenderService.java
+++ src/main/java/com/ekiauhce/sfpmock/client/SenderService.java
@@ -1,9 +1,12 @@
 package com.ekiauhce.sfpmock.client;
 
 import com.ekiauhce.sfpmock.api.objects.Doc;
+import com.ekiauhce.sfpmock.api.objects.Report;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 @Service
 public class SenderService {
@@ -11,8 +14,28 @@ public class SenderService {
     @Value("${receiver.url}")
     private String receiverUrl;
 
+    private final Random docRandom = new Random(19);
+    private final Random reportRandom = new Random(19);
+
+    public void sendDoc() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Doc doc = Doc.mock(
+                docRandom.nextLong(),
+                2000 + docRandom.nextInt(4000 - 2000)
+        );
+
+        restTemplate.postForObject(receiverUrl, doc, Doc.class);
+    }
+
     public void sendReport() {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject(receiverUrl, Doc.mock(), Doc.class);
+
+        Report report = Report.mock(
+                reportRandom.nextLong(),
+                2000 + reportRandom.nextInt(4000 - 2000)
+        );
+
+        restTemplate.postForObject(receiverUrl, report, Report.class);
     }
 }
